8148561490ee4b71bb9e74226bc1eb887fa6b1af
Nishant Singh Yadav <75436573+yadavNishant2020@users.noreply.github.com>
2024-06-03 01:31:03 +0530

    Update favicon and page title

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -19,7 +19,7 @@ function About() {
                             <a href="#contact">
                             <button
                                 type="button"
-                                className="self-center md:self-start focus:outline-none focus:ring-4 font-medium text-sm px-5 py-2.5 bg-gray-950 text-white border-gray-600 hover:bg-gray-700 hover:border-gray-600 focus:ring-gray-700">
+                                className=" self-center md:self-start border focus:outline-none  focus:ring-4  font-medium text-sm px-5 py-2.5 bg-gray-950 text-white border-gray-600 hover:bg-gray-700 hover:border-gray-600 focus:ring-gray-700">
                                 Contact Me
                             </button>
                             </a>
