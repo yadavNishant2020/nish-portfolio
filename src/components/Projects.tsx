2933adb67de3314eb124d3faaea7cacd450f73ea
Nishant Singh Yadav <75436573+yadavNishant2020@users.noreply.github.com>
2024-06-02 17:43:15 +0530

    Added mobile app data

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -9,7 +9,7 @@ function ProjectComponent({ project }: { project: Project }) {
     return (
         <div className="flex flex-col-reverse flex-wrap  justify-between lg:justify-normal min-w-[22rem] max-w-[40%] lg:max-w-full lg:flex-row items-center relative m-[5%] lg:m-0 lg:p-[7%] z-10 lg:py-[6%] bg-[#111111c0] lg:bg-transparent rounded-md shadow-[8px_5px_15px_-6px_#2d3748] lg:shadow-none">
             <div className=" w-full lg:w-[35vw] gap-6 flex flex-col p-4 lg:p-0">
-                <p className="text-lg pt-4 lg:pt-0">{project.id}</p>
+                <p className="text-lg pt-4 lg:pt-0">{`PROJECT #${project.id}`}</p>
                 <p className="text-xl font-semibold">{project.title}</p>
                 <div className="lg:bg-[#111111c0] lg:rounded-md lg:p-4 lg:shadow-[8px_5px_15px_-6px_#2d3748]">
                     <p className=" lg:text-lg">{project.description}</p>
@@ -29,7 +29,9 @@ function ProjectComponent({ project }: { project: Project }) {
                 </div>
             </div>
             <div className={`lg:absolute lg:w-1/2 right-[7%] transform transition-transform duration-300 ${isHovered ? 'z-20 scale-105' : '-z-10'}`} onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
-                <img className="rounded-t-md lg:rounded-md" src={project.image} alt={project.title} />
+                <a href={project.demoLink} target="_blank" rel="noopener noreferrer">
+                    <img className="rounded-t-md lg:rounded-md cursor-pointer" src={project.image} alt={project.title} />
+                </a>
             </div>
         </div>
     );
